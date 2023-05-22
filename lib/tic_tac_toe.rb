7d790a1e97d3d06028a412340534304d0eb89b7b
Brenda <brendakajuju777@gmail.com>
2023-05-22 14:33:15 +0300

    add valid move

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,4 +26,13 @@ class TicTacToe
         @board[index] = player_token
     end
 
-    
+    def position_taken? (index)
+        @board[index] != " "
+    end
+
+    def valid_move? (index)
+        index.between?(0, 8) && !position_taken(index)
+    end
+
+
+
