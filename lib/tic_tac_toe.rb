705fd6f1b987cf874df5c7e472142dcc82e1b5f2
Brenda <brendakajuju777@gmail.com>
2023-05-22 14:29:08 +0300

    add the display

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,29 @@
+class TicTacToe
+    def initialize
+        @board = Array.new(9, " ")
+    end
+
+    WIN_COMBINATIONS = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
+        [0, 4, 8], [2, 4, 6]              # Diagonals
+      ]
+    end
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index (input)
+        input.to_i - 1
+    end
+
+    def move (index, player_token = "X")
+        @board[index] = player_token
+    end
+
+    
